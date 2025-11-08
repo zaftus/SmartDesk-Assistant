909e7640e290713d50afd24ee5b48b3af00ca61c
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 22:29:13 -0500

    Create utils.py

---
+++ SmartDesk/utils.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+def load_json(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            return json.load(f)
+    return {}
+
+def save_json(file_path, data):
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=4)

2fd03f992e8a1345e95069df123dc7d6217de764
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 22:28:56 -0500

    Create logging_config.py

---
+++ SmartDesk/logging_config.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logger(name="SmartDesk"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+logger = setup_logger()
